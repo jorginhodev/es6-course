32f6df0423fbe8b1d382379b7f86689c3f7cf852
Jorge Ramos <jorginhodeveloper@gmail.com>
2018-06-06 14:05:27 -0300

    Add files about Modules in ES6

---
+++ 15-js-modules/app.js
@@ -0,0 +1,24 @@
+/* 
+  import método from 'biblioteca;
+  * carrega tudo da lib
+  as => alias para o método (novo nome)
+*/
+
+//import * as R from 'ramda';
+//import { union } from 'ramda';
+import { union as juntaTudo, uniq as soOsmesmos } from 'ramda';
+//import { union as juntaTudo } from 'ramda';
+
+const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
+const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1];
+
+//const arr3 = R.union(arr1, arr2);
+//const arr3 = union(arr1, arr2);
+const arr3 = juntaTudo(arr1, arr2);
+
+//const arr4= R.uniq(arr1);
+//const arr4= uniq(arr1);
+const arr4= soOsmesmos(arr1);
+
+console.log(arr3);
+console.log(arr4);
---
+++ 15-js-modules/webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    filename: './app.js'
+  },
+  output: {
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['es2015', { modules: false}]
+          ]
+        }
+      }
+    ]
+  }
+};
