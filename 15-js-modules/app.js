fba77e2034d5c7b4669cf9d81a8bc923ba8b4c61
Jorge Ramos <jorginhodeveloper@gmail.com>
2018-06-06 15:27:00 -0300

    Add exports

--- 15-js-modules/app.js
+++ 15-js-modules/app.js
@@ -4,21 +4,22 @@
   as => alias para o método (novo nome)
 */
 
-//import * as R from 'ramda';
-//import { union } from 'ramda';
 import { union as juntaTudo, uniq as soOsmesmos } from 'ramda';
-//import { union as juntaTudo } from 'ramda';
+
+import soma, { sub, multiplicacao, div as dividir, PI } from './utils';
+import { pipeline } from 'stream';
 
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
 const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1];
 
-//const arr3 = R.union(arr1, arr2);
-//const arr3 = union(arr1, arr2);
 const arr3 = juntaTudo(arr1, arr2);
-
-//const arr4= R.uniq(arr1);
-//const arr4= uniq(arr1);
 const arr4= soOsmesmos(arr1);
 
 console.log(arr3);
 console.log(arr4);
+
+console.log(soma(3, 2));
+console.log(sub(3, 2));
+console.log(multiplicacao(3, 2));
+console.log(dividir(4, 2));
+console.log(PI);
---
+++ 15-js-modules/utils.js
@@ -0,0 +1,29 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// named export
+// ter vários exports dentro de um msm arquivo
+// só pode chamar com o msm nome
+// import precisa das chaves { sub }
+export function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+const PI = 3.14
+
+export { mult as multiplicacao, div, PI };
+
+// método principal
+// só pode ter um default por arquivo
+// importar com qualquer nome
+// não precisa utilizar as chaves
+export default sum;
